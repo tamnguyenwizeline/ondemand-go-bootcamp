277f4b4c68d7c60b89b241e4d9829c3b9dfd5837
Tam <tam.nguyen@wizeline.com>
2023-07-04 07:58:53 +0700

    remove unused code

--- util/read_pokemons_csv.go
+++ util/read_pokemons_csv.go
@@ -40,37 +40,6 @@ func ReadCSVFile(filePath string) ([]model.Pokemon, error) {
 	return result, nil
 }
 
-func worker(id int, jobs <-chan int, results chan<- model.Pokemon, file *os.File, itemsPerWorker int) {
-	for j := range jobs {
-		fmt.Println("worker", id, "started  job", j)
-
-		reader := csv.NewReader(file)
-
-		// Read and process the specified number of items
-		// Read the record
-		record, err := reader.Read()
-		if err != nil {
-			// Handle any error in reading the CSV file
-			fmt.Println("Error reading CSV file:", err)
-			return
-		}
-		fmt.Println("Record", record)
-
-		// Process the record
-		id, err := strconv.Atoi(record[0])
-		if err != nil {
-			fmt.Printf("Invalid ID: %v\n", record[0])
-			return
-		}
-
-		pokemon := model.Pokemon{
-			ID:   id,
-			Name: record[1],
-		}
-		results <- pokemon
-	}
-}
-
 func ReadCSVDataWithWorkers(filePath string, items, itemsPerWorker int) ([]*model.Pokemon, error) {
 	file, err := os.Open(filePath)
 	response := make([]*model.Pokemon, 0)
@@ -90,7 +59,7 @@ func ReadCSVDataWithWorkers(filePath string, items, itemsPerWorker int) ([]*mode
 	worker := func(jobs <-chan []string, results chan<- *model.Pokemon) {
 		for {
 			select {
-			case job, ok := <-jobs: // you must check for readable state of the channel.
+			case job, ok := <-jobs:
 				if !ok {
 					return
 				}
@@ -102,7 +71,6 @@ func ReadCSVDataWithWorkers(filePath string, items, itemsPerWorker int) ([]*mode
 	for w := 0; w < totalWorkers; w++ {
 		wg.Add(1)
 		go func() {
-			// this line will exec when chan `res` processed output at line 107 (func worker: line 71)
 			defer wg.Done()
 			worker(jobs, result)
 		}()
